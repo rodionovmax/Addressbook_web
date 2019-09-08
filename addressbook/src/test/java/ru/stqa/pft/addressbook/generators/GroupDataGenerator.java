4bed3d0f78eda426b842f6a27dfc2ca95289afce
max <rodionovmax90test@gmail.com>
2019-09-08 12:59:53 -0400

    Lesson 6.7 Methods to generate and read json files 1. In GroupDataGenerator created method that generates json file using Gson library 2. In GroupCreationTests created method to read json file and use it as parameters using Gson library

--- addressbook/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -41,11 +43,21 @@ public class GroupDataGenerator {
         List<GroupData> groups = generateGroups(count);
         if (format.equals("csv")){
             saveAsCsv(groups, new File(file));
-        } else if (format.equals("xml")){
+        } else if (format.equals("xml")) {
             saveAsXml(groups, new File(file));
+        } else if (format.equals("json")){
+            saveAsJson(groups, new File(file));
         } else {
             System.out.println("Unrecognized format " + format);
         }
+    }
+
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
     }
 
     private void saveAsXml(List<GroupData> groups, File file) throws IOException {
--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,8 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
-import org.hamcrest.MatcherAssert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -44,7 +45,7 @@ public class GroupCreationTests extends TestBase {
     }
 
     @DataProvider
-    public Iterator<Object[]> validGroupsUsingCsvFile() throws IOException {
+    public Iterator<Object[]> validGroupsFromCsv() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         BufferedReader reader = new BufferedReader(new FileReader(new File("/Users/max/sandbox/Addressbook/addressbook/src/test/resources/groups.csv")));
         String line = reader.readLine();
@@ -57,7 +58,7 @@ public class GroupCreationTests extends TestBase {
     }
 
     @DataProvider
-    public Iterator<Object[]> validGroupsUsingXmlFile() throws IOException {
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("/Users/max/sandbox/Addressbook/addressbook/src/test/resources/groups.xml")));
         String xml = "";
         String line = reader.readLine();
@@ -66,13 +67,26 @@ public class GroupCreationTests extends TestBase {
             line = reader.readLine();
         }
         XStream xstream = new XStream(new DomDriver());
-//        XStream xstream = new XStream();
         xstream.processAnnotations(GroupData.class);
         List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
         return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validGroupsUsingXmlFile")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("/Users/max/sandbox/Addressbook/addressbook/src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreationEdited(GroupData group){
             app.goTo().groupPage();
             Groups before = app.group().all();
